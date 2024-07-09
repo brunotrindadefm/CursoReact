7d30b19a6f787242e95d7e88dc471aa8fc7a8c8b
btrindadefm <btrindade172@gmail>
2024-07-09 12:44:43 -0300

    Tirando o desnecessário

--- src/App.jsx
+++ src/App.jsx
@@ -2,13 +2,10 @@ import { BrowserRouter as Router, Route, Routes, Link } from 'react-router-dom'
 import './index.css'
 import Home from './components/pages/Home'
 import NewProject from './components/pages/NewProject'
-import Company from './components/pages/Company'
-import Contact from './components/pages/Contact'
 import Navbar from './components/layout/Navbar'
 import Footer from './components/layout/Footer'
 import Projects from './components/pages/Projects'
 import Project from './components/pages/Project'
-
 import Container from './components/layout/Container'
 
 function App() {
@@ -21,8 +18,6 @@ function App() {
           <Routes>
             <Route path='/' element={<Home />} />
             <Route path='/projects' element={<Projects />} />
-            <Route path='/company' element={<Company />} />
-            <Route path='/contact' element={<Contact />} />
             <Route path='/newprojects' element={<NewProject />} />
             <Route path='/project/:id' element={<Project />} />
           </Routes>
